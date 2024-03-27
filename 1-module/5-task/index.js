21ccf5cf11cc677f9f7078da121d83cf42b024dd
AkiraNewzoo <newzor48@yandex.ru>
2024-03-28 00:12:48 +0300

    5 task complete

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  // Проверка длины строки
+  if (str.length <= maxlength) {
+    return str;
+  }
+
+  // Усечение строки
+  const newStr = str.substring(0, maxlength - 1);
+
+  // Добавление знака троеточия
+  return newStr + "…";
 }
