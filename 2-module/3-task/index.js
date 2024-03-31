03442f6251dcd6d315ad4fb5208cfe4730cc21cf
AkiraNewzoo <newzor48@yandex.ru>
2024-03-31 14:03:40 +0300

    task 2/3 done

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,25 @@
 let calculator = {
-  // ваш код
+  //Метод для чтения и сохранения значений
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  //Метод для сложения
+  sum() {
+  return this.a + this.b;
+  },
+  //Метод для умножения
+  mul() {
+    return this.a * this.b;
+  },
+  
+  /*Пример из учебника не рабочий
+  read() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  },*/
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
